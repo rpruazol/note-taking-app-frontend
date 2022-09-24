21bcc3e90e1d879a2717ff4de95527e88a55b4b0
rpruazol <rpruazol@gmail.com>
2022-09-23 20:18:20 -0400

    scaffolding, modal and header

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Note from './Note'
+
+function Header() {
+  const [modalOpen, showModal] = useState(false);
+  
+  return (
+    <> 
+       <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+            </Nav>
+            <Nav md={{ span: 4, offset: 4 }}>
+              <Nav.Link onClick={() => showModal(true)}>New Note</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Note 
+        show={modalOpen}
+        showModal={showModal}
+      />
+    </>
+  );
+}
+
+export default Header;
