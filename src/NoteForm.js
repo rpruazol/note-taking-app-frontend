dd43d8f80349aaa83bd54b89a327dbefb78f5ac7
rpruazol <rpruazol@gmail.com>
2022-09-25 20:53:25 +0000

    create notes

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,26 +1,63 @@
 import Form from 'react-bootstrap/Form';
-import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export default function NoteForm(props) {
+  const [noteTitle, setTitle] = useState('');
+  const [noteDescription, setDescription] = useState('');
+
+
+  const createNote = async (e) => {
+    e.preventDefault()
+    console.log('noteTitle ', noteTitle);
+    console.log('noteDescription ', noteDescription);
+    const config = {
+      url: '/newnote',
+      method: 'post',
+      baseURL: process.env.REACT_APP_BACKEND,
+      data: {
+        title: noteTitle,
+        description: noteDescription
+      }
+
+    }
+    const response = await axios(config);
+
+    console.log(response)
+  }
+
 
 
   return (
-    <Form onSubmit={(e) => props.createNote(e)}>
-    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form onSubmit={(e) => createNote(e)}>
+    <Form.Group className="mb-3" controlId="note-title">
       <Form.Label>Note Title</Form.Label>
       <Form.Control
         type="text"
         autoFocus
+        onChange={(e) => setTitle(e.target.value)}
       />
     </Form.Group>
-    <Form.Group
-      className="mb-3"
-      controlId="exampleForm.ControlTextarea1"
-    >
-      <Form.Label>Description</Form.Label>
-      <Form.Control as="textarea" rows={3} />
+    <Form.Group className="mb-3" controlId="note-description">
+
+    <Form.Label>Description</Form.Label>
+    <Form.Control 
+      as="textarea" 
+      rows={3}
+      onChange={(e) => setDescription(e.target.value)} />
     </Form.Group>
+
+    <Modal.Footer>
+
+      <Button variant="secondary" onClick={() => props.showModal(false)}>
+        Close
+      </Button>
+      <Button variant="primary" type="submit">
+        Add Note
+      </Button>
+    </Modal.Footer>
   </Form>
   );
 }
--- src/NoteModal.js
+++ src/NoteModal.js
@@ -6,29 +6,6 @@ import Form from 'react-bootstrap/Form'
 import axios from 'axios'
 
 export default function Note(props) {
-  const [noteTitle, setTitle] = useState('');
-  const [noteDescription, setDescription] = useState('');
-
-
-  const createNote = async (e) => {
-    e.preventDefault()
-    console.log('noteTitle ', noteTitle);
-    console.log('noteDescription ', noteDescription);
-    const config = {
-      url: '/newnote',
-      method: 'post',
-      baseURL: process.env.REACT_APP_BACKEND,
-      data: {
-        title: noteTitle,
-        description: noteDescription
-      }
-
-    }
-    const response = await axios(config);
-
-    console.log(response)
-  }
-
 
   return (
     <>
@@ -37,34 +14,8 @@ export default function Note(props) {
           <Modal.Title>Add a new Note!</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={(e) => createNote(e)}>
-            <Form.Group className="mb-3" controlId="note-title">
-              <Form.Label>Note Title</Form.Label>
-              <Form.Control
-                type="text"
-                autoFocus
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="note-description">
-
-            <Form.Label>Description</Form.Label>
-            <Form.Control 
-              as="textarea" 
-              rows={3}
-              onChange={(e) => setDescription(e.target.value)} />
-            </Form.Group>
-
-            <Modal.Footer>
-
-              <Button variant="secondary" onClick={() => props.showModal(false)}>
-                Close
-              </Button>
-              <Button variant="primary" type="submit">
-                Add Note
-              </Button>
-            </Modal.Footer>
-          </Form>
+      <NoteForm
+      />
         </Modal.Body>
       </Modal>
     </>
